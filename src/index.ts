91999040ffd2946a1ff70f661c26784df87195e4
leo-jansen <janseleo@gmail.com>
2021-12-06 19:10:28 -0300

    config bot

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import {create, Message, Whatsapp} from 'venom-bot'
+import {config} from 'dotenv'
+
+config()
+
+create('teste')
+.then(client => start(client))
+.catch(erro => console.error(erro))
+
+async function start(client:Whatsapp) {
+  client.onMessage(async (message: Message) => {
+    let chatID = String(process.env.CHAT_ID);
+    let autor = String(process.env.AUTOR)
+    let alvo = String(process.env.ALVO);
+    if (message.chat.isGroup == true && message.chat.id == chatID && message.author == autor) {
+      await client.sendText(alvo, message.body)
+      .then(result => console.log(result))
+      .catch(erro => console.error(erro))
+    }
+  })
+}
